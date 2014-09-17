bc62ec6d76da2fb8670748c9eb61a89f9bc262e9
sirmordred <bambu_bambu@windowslive.com>
2014-09-17 23:42:53 +0300

    İnitial MahdiRom Configuration

--- mahdi.mk
+++ mahdi.mk
@@ -1,5 +1,6 @@
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Mahdi stuff.
+$(call inherit-product, vendor/mahdi/configs/common.mk)
+$(call inherit-product, vendor/mahdi/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, $(LOCAL_PATH)/full_ancora.mk)
@@ -7,10 +8,14 @@ $(call inherit-product, $(LOCAL_PATH)/full_ancora.mk)
 PRODUCT_RELEASE_NAME := ancora
 
 # Setup device configuration
-PRODUCT_NAME := cm_ancora
+PRODUCT_NAME := mahdi_ancora
 PRODUCT_DEVICE := ancora
 PRODUCT_BRAND := Samsung
 PRODUCT_MANUFACTURER := Samsung
 PRODUCT_MODEL := GT-I8150
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=samsung/GT-I8150/GT-I8150:2.3.6/GINGERBREAD/XXLMJ:user/release-keys PRIVATE_BUILD_DESC="GT-I8150-user 2.3.6 GINGERBREAD XXLMJ release-keys"
+
+# Copy device specific prebuilt files.
+PRODUCT_COPY_FILES += \
+ vendor/mahdi/prebuilt/bootanimations/BOOTANIMATION-800x480.zip:system/media/bootanimation.zip
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -18,4 +18,4 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
-add_lunch_combo cm_ancora-userdebug
+add_lunch_combo mahdi_ancora-userdebug
